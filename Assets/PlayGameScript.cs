0d937671ec77af223c80af428f08b318f4488446
BaciTesting <125468626+BaciTesting@users.noreply.github.com>
2024-11-09 14:12:08 +0100

    MainMenu

---
+++ Assets/PlayGameScript.cs
@@ -0,0 +1,15 @@
+using TMPro.Examples;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayGameScript : MonoBehaviour
+{
+   public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
