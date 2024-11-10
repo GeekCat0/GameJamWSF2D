5d22948d5d64db8e2e816281d0969af78fc3f2e2
waterrbottle <112860891+waterrbottle@users.noreply.github.com>
2024-11-10 11:48:54 +0100

    cd

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -8,12 +8,12 @@ public class spawner : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for(int i = 0; i < 1500; i++)
+        for(int i = 0; i < 1000; i++)
         {
            Vector3 position = new Vector2(Random.Range(-50.0F, 50.0F), Random.Range(-50.0F, 50.0F));
 	        Instantiate(grass, position, transform.rotation);
         }
-        for(int i = 0; i < 1500; i++)
+        for(int i = 0; i < 1000; i++)
         {
            Vector3 position = new Vector2(Random.Range(-50.0F, 50.0F), Random.Range(-50.0F, 50.0F));
 	        Instantiate(grass2, position, transform.rotation);
